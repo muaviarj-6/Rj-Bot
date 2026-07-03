6e943ded48343f157013207a6ef9ffdb685058f5
muaviarj-6 <muaviarj6@gmail.com>
2026-07-03 19:13:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
 
+const { Client, GatewayIntentBits, Collection, Events } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.commands = new Collection();
+
+client.once(Events.ClientReady, () => {
+  console.log(`✅ ${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
