f1854706f05668beeab414e2042ba71558476d77
muaviarj-6 <muaviarj6@gmail.com>
2026-07-04 09:57:00 +0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,38 @@
-const { Events } = require("discord.js");
+const {
+  Events,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
 
-  execute(client) {
+  async execute(client) {
     console.log(`✅ ${client.user.tag} is online!`);
+
+    const commands = [
+      new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with Pong!")
+        .toJSON(),
+    ];
+
+    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+    try {
+      await rest.put(
+        Routes.applicationGuildCommands(
+          "1393946603001221220",
+          "1519649134938816564"
+        ),
+        { body: commands }
+      );
+
+      console.log("✅ Slash commands registered!");
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
