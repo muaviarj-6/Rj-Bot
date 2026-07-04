d3d732bcd6eca5006126f4e6af886ceb08c830b3
muaviarj-6 <muaviarj6@gmail.com>
2026-07-04 09:32:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits, Collection, Events } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -8,8 +10,28 @@ const client = new Client({
 
 client.commands = new Collection();
 
-client.once(Events.ClientReady, () => {
-  console.log(`✅ ${client.user.tag} is online!`);
-});
+// Load Commands
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+  console.log(`Loaded command: ${command.data.name}`);
+}
+
+// Load Events
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(process.env.TOKEN);
