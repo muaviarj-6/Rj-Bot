0a91635250efd051582a416559dac96e688eefd0
muaviarj-6 <muaviarj6@gmail.com>
2026-07-04 09:29:27 +0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,31 @@
+const { Events } = require("discord.js");
 
+module.exports = {
+  name: Events.InteractionCreate,
+
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "❌ An error occurred while executing this command.",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "❌ An error occurred while executing this command.",
+          ephemeral: true,
+        });
+      }
+    }
+  },
+};
