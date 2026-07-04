cad2187b57832e6b1e289d61ebe7176e1d5ea900
muaviarj-6 <muaviarj6@gmail.com>
2026-07-04 09:28:34 +0500

    Update loadCommands.js

--- handlers/loadCommands.js
+++ handlers/loadCommands.js
@@ -1,1 +1,16 @@
+const fs = require("fs");
+const path = require("path");
 
+module.exports = (client) => {
+  const commandsPath = path.join(__dirname, "../commands");
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+
+    if (command.data && command.execute) {
+      client.commands.set(command.data.name, command);
+      console.log(`✅ Loaded command: ${command.data.name}`);
+    }
+  }
+};
