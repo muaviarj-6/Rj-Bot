25dacd5afd341b1c64587d225814dd217c75fe61
muaviarj-6 <muaviarj6@gmail.com>
2026-07-04 09:30:15 +0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,10 @@
+const { Events } = require("discord.js");
 
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+
+  execute(client) {
+    console.log(`✅ ${client.user.tag} is online!`);
+  },
+};
